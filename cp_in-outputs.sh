2df9bdba393aa1641eb73e648334cc3eed360dd2
arturesteves <arturmesteves@hotmail.com>
2018-02-28 05:27:17 +0000

    no message

--- cp_in-outputs.sh
+++ cp_in-outputs.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [[ ! -e /usr/src/my-app/inputs && ! -e /usr/src/my-app/outputs]] then
-	mkdir /usr/src/my-app/inputs
-	mkdir /usr/src/my-app/outputs
-fi
-
-cp /tmp/sirs/intro/inputs/* /usr/src/my-app/inputs
-cp /tmp/sirs/intro/outputs/* /usr/src/my-app/outputs
+mkdir -p /usr/src/my-app/inputs && cp /tmp/sirs/intro/inputs/* /usr/src/my-app/inputs
+mkdir -p /usr/src/my-app/outputs && cp /tmp/sirs/intro/outputs/* /usr/src/my-app/outputs
