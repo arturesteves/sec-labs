210f1b4579687b5fdab853ffcd7a9ba5825810ec
arturesteves <arturmesteves@hotmail.com>
2018-02-28 03:03:21 +0000

    no message

--- cp_in-outputs.sh
+++ cp_in-outputs.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-cp /tmp/sirs/intro/inputs /usr/src/my-app/inputs
-cp /tmp/sirs/intro/outputs /usr/src/my-app/outputs
+cp /tmp/sirs/intro/inputs/* /usr/src/my-app/inputs
+cp /tmp/sirs/intro/outputs/* /usr/src/my-app/outputs
